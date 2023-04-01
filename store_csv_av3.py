48d5594267fc1f750e9bcc13e0c131c4c3fde43f
Hallen Victor <101992979+hallen1995@users.noreply.github.com>
2023-04-01 00:37:56 -0300

    stores_csv_av3_largada

---
+++ store_csv_av3.py
@@ -0,0 +1,17 @@
+    
+import pandas as pd
+
+tabela = pd.read_csv("C:/Users/usuario/Desktop/store_csv_av3/Stores.csv", sep=',')
+# print(tabela)
+
+tabela.columns.values
+tabelacolumns = ['Store ID ', 'Store_Area', 'Items_Available', 'Daily_Customer_Count', 'Store_Sales']
+tabelaFiltrado = tabela.filter(items=tabelacolumns)
+tabelaFiltrado = tabelaFiltrado.rename(columns={
+    'Store ID ' : 'ID',
+    'Store_Area' : 'Itens',
+    'Items_Available' : 'Disponíveis',
+    'Daily_Customer_Count' : 'Visitantes',
+    'Store_Sales' : 'Vendas (dólar)'
+})
+print(tabelaFiltrado)
